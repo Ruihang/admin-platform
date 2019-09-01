724a72f2ea029dbfaedd0936fb18fba7ff5d6400
Ruihang <ruihang@foxmail.com>
2019-09-01 19:59:23 +0800

    add consul

--- mongo-admin/src/main/java/com/louis/mongo/admin/MongoAdminApplication.java
+++ mongo-admin/src/main/java/com/louis/mongo/admin/MongoAdminApplication.java
@@ -2,7 +2,9 @@ package com.louis.mongo.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"com.louis.mongo"})
 public class MongoAdminApplication {
 
--- mongo-monitor/src/main/java/com/louis/mongo/monitor/MongoMonitorApplication.java
+++ mongo-monitor/src/main/java/com/louis/mongo/monitor/MongoMonitorApplication.java
@@ -1,9 +1,13 @@
 package com.louis.mongo.monitor;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(scanBasePackages = {"com.louis.mongo"})
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
 public class MongoMonitorApplication {
 
 	public static void main(String[] args) {
