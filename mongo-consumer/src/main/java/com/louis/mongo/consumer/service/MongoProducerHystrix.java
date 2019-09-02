16439790abc314ec942ad68af4fd3cc6fbdc6a42
Ruihang <ruihang@outlook.com>
2019-09-03 00:48:13 +0800

    update user.email commit

--- mongo-consumer/src/main/java/com/louis/mongo/consumer/service/MongoProducerHystrix.java
+++ mongo-consumer/src/main/java/com/louis/mongo/consumer/service/MongoProducerHystrix.java
@@ -1,4 +1,14 @@
 package com.louis.mongo.consumer.service;
 
-public class MongoProducerHystrix {
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Component
+public class MongoProducerHystrix implements MongoProducerService {
+
+    @Override
+    @RequestMapping("/hello")
+    public String hello() {
+        return "sorry, hello service call failed.";
+    }
 }
--- mongo-consumer/src/main/java/com/louis/mongo/consumer/service/MongoProducerService.java
+++ mongo-consumer/src/main/java/com/louis/mongo/consumer/service/MongoProducerService.java
@@ -3,7 +3,7 @@ package com.louis.mongo.consumer.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "mongo-producer")
+@FeignClient(name = "mongo-producer", fallback = MongoProducerHystrix.class)
 public interface MongoProducerService {
 
     @RequestMapping("/hello")
